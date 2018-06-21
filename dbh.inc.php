0c767b4602226285be6d4b82c19dacbb78db967e
Zcool99 <zachary.ehle@gmail.com>
2018-06-21 15:49:29 -0400

    Add files via upload

---
+++ dbh.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbserverName = "localhost";
+$dbuserName = "root";
+$dbpassword = "root";
+$dbname = "loginsystem";
+
+$conn = mysqli_connect( $dbserverName , $dbuserName , $dbpassword , $dbname );
